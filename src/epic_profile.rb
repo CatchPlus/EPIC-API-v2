83d9f4e8fe23216c1bf2ee1a9bb147b2714948b0
Pieter van Beek <pieter@djinnit.com>
2012-11-26 12:52:52 +0100

    Added profile GWDGPIDAuth

--- src/epic_profile.rb
+++ src/epic_profile.rb
@@ -95,6 +95,86 @@ class Profile < Resource
   end # class NoDelete < Profile
 
 
+  # A profile that uses UUIDs to guarantee the uniqueness of created Handles.
+  class GWDGOLDPIDAuth < Profile
+
+
+    def to_rackful
+      {
+        'Description' => 'This profile restricts access to a subspace of the PID namespace.',
+      }
+    end
+
+
+    # This method validates the creation of a new handle.
+    # 
+    # The method can not only veto the creation of a handle, but also allow
+    # handle creation, but with modified handle values.
+    # @param [Rackful::Request] request
+    # @param [String] prefix
+    # @param [String] suffix
+    # @param [(HandleValue)] values
+    # @return [(HandleValue), nil] The (possibly modified) array of
+    #   {HandleValue HandleValues} to put in the new {Handle}.
+    # @raise [Rackful::HTTPStatus] if creation cannot pass.
+    def create( request, prefix, suffix, values )
+      return nil unless GWDGPID_PREFIXES.include? prefix
+      unless inst = USERS[request.env['REMOTE_USER']][:institute]
+        raise Rackful::HTTP403Forbidden, "You don't have an institute code set."
+      end
+      inst.upcase!
+      unless 0 == suffix.index("00-#{inst}-")
+        raise Rackful::HTTP403Forbidden, "You can only create PIDs starting with #{prefix}/00-#{inst}-"
+      end
+      nil
+    end
+
+
+    # @!method 
+    # This method validates the update of an existing handle.
+    # 
+    # The method can not only veto the creation of a handle, but also allow
+    # handle creation, but with modified handle values.
+    # @param request [Rackful::Request]
+    # @param prefix [String]
+    # @param suffix [String]
+    # @param old_values [(HandleValue)]
+    # @param new_values [(HandleValue)]
+    # @return [(HandleValue), nil] The (possibly modified) array of
+    #   {HandleValue HandleValues} to put in the new {Handle}.
+    # @raise [Rackful::HTTPStatus] if the update cannot pass.
+    def update( request, prefix, suffix, old_values, new_values )
+      return nil unless GWDGPID_PREFIXES.include? prefix
+      unless inst = USERS[request.env['REMOTE_USER']][:institute]
+        raise Rackful::HTTP403Forbidden, "You don't have an institute code set."
+      end
+      inst.upcase!
+      unless 0 == suffix.index("00-#{inst}-")
+        raise Rackful::HTTP403Forbidden, "You can only update PIDs starting with #{prefix}/00-#{inst}-"
+      end
+      nil
+    end
+
+
+    # This method must validate the deletion of a handle.
+    # @param handle [Handle]
+    # @return [void]
+    # @raise [Rackful::HTTPStatus] if the deletion cannot pass.
+    def delete( request, prefix, suffix, old_values )
+      return unless GWDGPID_PREFIXES.include? prefix
+      unless inst = USERS[request.env['REMOTE_USER']][:institute]
+        raise Rackful::HTTP403Forbidden, "You don't have an institute code set."
+      end
+      inst.upcase!
+      unless 0 == suffix.index("00-#{inst}-")
+        raise Rackful::HTTP403Forbidden, "You can only delete PIDs starting with #{prefix}/00-#{inst}-"
+      end
+    end
+
+
+  end # class NoDelete < Profile
+
+
 end # class Profile
 
 
